f142d939f9294db15d697d397dfa189b69b2d7ea
fobo4ka <fobo4ka@gmail.com>
2015-08-28 15:41:24 +0300

    Initial commit

---
+++ js/MainController.js
@@ -0,0 +1,61 @@
+app.controller('MainController', ['$scope', function($scope) {
+
+	$scope.products = [];
+	$scope.emptyCart = true;
+  
+  	$scope.addProduct = function() {
+  		if (($scope.name != "")&&($scope.price != "")&&(!isNaN($scope.price))&&($scope.price > 0)) {
+	    	$scope.products.push({
+				name: $scope.name,
+				price: $scope.price,
+				priceWithDiscount: ''
+	    	});
+	    	$scope.emptyCart = false;
+        $scope.error = false;
+  			$scope.addDiscount();	    
+  		}
+      else {
+        $scope.error = true;      
+      }
+    	$scope.name = '';
+    	$scope.price = '';
+  };
+
+  	$scope.addDiscount = function() {
+  		var commonCost = 0;	
+  		var maxPrice = 0;
+  		var commonDiscount = 0;
+
+      if (($scope.discount != "")&&(!isNaN($scope.discount))&&($scope.discount >= 0)) {
+        var discount = parseInt($scope.discount);
+      }
+      else {
+        var discount = $scope.discount = 0;
+      }
+
+   		angular.forEach($scope.products, function(product){
+   			var price = parseInt(product.price);
+   			if (price > maxPrice) {
+   				maxPrice = price;
+   			}
+  			commonCost += price;
+  		});
+
+  		angular.forEach($scope.products, function(product){
+  			var price = parseInt(product.price);
+  			if (price != maxPrice) {
+  				var priceWithDiscount = Math.floor(price - price/(commonCost/discount));
+				product.priceWithDiscount = (priceWithDiscount >= 0  ? priceWithDiscount : 0);
+  				commonDiscount += price - product.priceWithDiscount; 
+  			}
+  		});
+
+		angular.forEach($scope.products, function(product){
+  			var price = parseInt(product.price);
+  			if (price == maxPrice) {
+  				var priceWithDiscount = price - (discount - commonDiscount);
+				product.priceWithDiscount = (priceWithDiscount >= 0  ? priceWithDiscount : 0);
+  			}
+  		});		
+  	};
+}]);
